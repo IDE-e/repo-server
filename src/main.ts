f21aab42ec89dc7c95d60850f6c1b16d9c122bd3
yeoniii20 <100350818+yeoniii20@users.noreply.github.com>
2025-12-23 15:26:45 +0900

    feat: Enhance CORS and set global API prefix
    Updated CORS settings and added global prefix for API routes.

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,30 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // CORS 허용
+  // client 3000
+  // server 4000
   app.enableCors({
-    origin: ['http://localhost:3000'],
-    credentials: true,
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   });
+
+  app.setGlobalPrefix("api");
 
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
-    }),
+      forbidNonWhitelisted: true,
+    })
   );
 
+  // Next랑 3000 포트 겹치기 때문에 4000 포트로 변경
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
